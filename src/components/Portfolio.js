98e2a739535cde12ed2ceb92f68da4ce71eadb50
David Tunnell <davidtunnell9@gmail.com>
2021-10-17 14:08:21 -0500

    added map/loop logic to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -81,102 +81,29 @@ const Portfolio = () => {
                                 data-loop="true"
                                 data-center="true"
                             >
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>California, USA</h3>
-                                        </div>
-                                    </a>
-                                </article>
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>Oahu, Hawaii</h3>
-                                        </div>
-                                    </a>
-                                </article>
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>Bali, Indonesia</h3>
-                                        </div>
-                                    </a>
-                                </article>
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>Arizona, USA</h3>
-                                        </div>
-                                    </a>
-                                </article>
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>Yellowstone, USA</h3>
-                                        </div>
-                                    </a>
-                                </article>
-                                <article class="tile tile-long">
-                                    <div class="tile-image"></div>
-                                    <a href="" class="tile-content">
-                                        <div class="tile-header on-hover text-right">
-                                            <h4 class="fs-30 text-white">
-                                                $259
-                                            </h4>
-                                        </div>
-                                        <div class="tile-footer">
-                                            <span class="eyebrow">
-                                                5 Day City Tour
-                                            </span>
-                                            <h3>Cappadocia, Turkey</h3>
-                                        </div>
-                                    </a>
-                                </article>
+                                {portfolio.map((element) => (
+                                    <article class="tile tile-long">
+                                        <div
+                                            class="tile-image"
+                                            style={{
+                                                backgroundImage: `url(${element.image})`,
+                                            }}
+                                        ></div>
+                                        <a href="/" class="tile-content">
+                                            <div class="tile-header on-hover text-right">
+                                                <h4 class="fs-30 text-white">
+                                                    {element.topRight}
+                                                </h4>
+                                            </div>
+                                            <div class="tile-footer">
+                                                <span class="eyebrow">
+                                                    {element.flavor}
+                                                </span>
+                                                <h3>{element.title}</h3>
+                                            </div>
+                                        </a>
+                                    </article>
+                                ))}
                             </div>
                         </div>
                     </div>
